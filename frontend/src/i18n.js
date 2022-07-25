f65c097356b7ab2a6c1153e3798f6876556cf068
aylinguldur <aylin.guldur@gmail.com>
2022-07-25 22:48:01 +0300

    fixed i18next.js problem

--- frontend/src/i18n.js
+++ frontend/src/i18n.js
@@ -1,45 +1,32 @@
 import i18n from 'i18next';
-import { initReactI18next } from 'react-i18next';
+import {initReactI18next} from 'react-i18next';
+import translationsEN from "../src/locales/en/translation.json";
+import translationsTR from "../src/locales/tr/translation.json";
 
+
+const resources = {
+    en: {
+        translations: translationsEN,
+    },
+    tr: {
+        translations: translationsTR,
+    },
+}
 i18n.use(initReactI18next).init({
-    resources:{
-        en:{
-            translations:{
-                'Sign Up':'Sign Up',
-                'password mismatch':'password mismatch',
-                'Username':'Username',
-                'Display Name':'Display Name',
-                'Password':'Password',
-                'Password Repeat':'Password Repeat',
-                'Login' : 'Login'
-
-            }
-        },
-        tr:{
-            translations:{
-                'Sign Up':'Kayıt Ol',
-                'password mismatch':'Şifreleriniz eşleşmiyor',
-                'Username':'Kullanıcı adı',
-                'Display Name':'Tercih Edilen İsim',
-                'Password':'Şifre',
-                'Password Repeat':'Şifre Tekrar',
-                'Login':'Giriş Yap'
-            }
-        },
-
-        fallbackLng:'tr',
-        ns:['translations'],
-        defaultNS:'translations',
-        keySeperator:false,
-        interpolation:{
-            escapeValue:false,
-            formatSeperator: ','
-        },
-        react:{
-            wait:true
-        }
+    resources,
+    fallbackLng: 'tr',
+    ns: ['translations'],
+    defaultNS: 'translations',
+    keySeperator: false,
+    interpolation: {
+        escapeValue: false,
+        formatSeperator: ','
+    },
+    react: {
+        wait: true
     }
-});
+})
+;
 
 
-export  default i18n;
+export default i18n;
